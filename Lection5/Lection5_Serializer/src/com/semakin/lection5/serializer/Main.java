5ca57678f941ec616c97ed2606f7765b8db13993
Chi <chipsetone@gmail.com>
2017-02-11 10:01:04 +0300

    немного отрефакторил

--- Lection5/Lection5_Serializer/src/com/semakin/lection5/serializer/Main.java
+++ Lection5/Lection5_Serializer/src/com/semakin/lection5/serializer/Main.java
@@ -7,25 +7,23 @@ import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.transform.TransformerException;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.io.Serializable;
 
-/**
- * Created by Chi on 10.02.2017.
- */
 public class Main {
 
     public static void main(String[] args) throws IllegalAccessException, TransformerException, ParserConfigurationException, IOException {
         ISerializatorable<String> serializator = new ReflectionSerializator();
 
         People people = new People("peopleName",100500, 35, "publicInfo");
-        String xmlPeople = serializator.serialize(people);
-        System.out.println(xmlPeople);
-        writeToXml(xmlPeople, "People.xml");
+        writeToXml(serializator, people, "People.xml");
 
         Student student = new Student("john doe", 18);
-        String xmlStudent = serializator.serialize(student);
-        System.out.println(xmlStudent);
-        writeToXml(xmlStudent, "Student.xml");
+        writeToXml(serializator, student, "Student.xml");
+    }
+
+    private static void writeToXml(ISerializatorable<String> serializator, Object obj, String fileName) throws IOException, IllegalAccessException, TransformerException, ParserConfigurationException {
+        String xmlObject = serializator.serialize(obj);
+        System.out.println(xmlObject);
+        writeToXml(xmlObject, fileName);
     }
 
     private static void writeToXml(String fileContent, String fileName) throws IOException {
