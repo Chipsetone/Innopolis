12162ef4a7173f17b0995243eda13d1e5b56d2c9
Chi <chipsetone@gmail.com>
2017-02-12 10:19:17 +0300

    Удалил лишнее

--- Tasks/Task1/src/com/semakin/Main.java
+++ Tasks/Task1/src/com/semakin/Main.java
@@ -1,8 +1,6 @@
 package com.semakin;
 
 public class Main {
-
-
     /*Необходимо разработать программу,
     которая получает на вход список ресурсов,
     содержащих набор чисел и считает сумму всех положительных четных.
@@ -12,10 +10,9 @@ public class Main {
     Все ошибки должны быть корректно обработаны, все API покрыто модульными тестами*/
 
     public static void main(String[] args) {
-
+        // в папке тестов есть генератор тестовых валидных файлов, запускаемый из TestFilesGenerateMain.java
         ResultPrinter printer = new ResultPrinter();
         ApplicationFacade application = new ApplicationFacade(printer);
         application.Run(args);
-
     }
 }
