750f5efb5d86460224aa681156fdabf5c3983360
Chi <chipsetone@gmail.com>
2017-02-15 10:42:38 +0300

    ClassLoader - добавил примитивный класс

---
+++ Lections/Lection7_3/Lection7_3_ClassLoader/src/main/java/Main.java
@@ -0,0 +1,18 @@
+import com.semakin.pojo.Animal;
+
+/**
+ * @author Семакин Виктор
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        Animal animal = new Animal("beaver");
+
+        System.out.println("do something in main");
+
+        animal.eat();
+        animal.doCrap();
+        animal.sleep();
+
+    }
+}
---
+++ Lections/Lection7_3/Lection7_3_ClassLoader/src/main/java/com/semakin/pojo/Animal.java
@@ -0,0 +1,28 @@
+package com.semakin.pojo;
+
+/**
+ * @author Семакин Виктор
+ */
+public class Animal {
+    private String name;
+
+    public Animal(String name) {
+        this.name = name;
+    }
+
+    public void eat(){
+        print(name + " eating");
+    }
+
+    public void sleep(){
+        print(name + " sleeping");
+    }
+
+    public void doCrap(){
+        print(name + " craping");
+    }
+
+    private void print(String message){
+        System.out.println(message);
+    }
+}
