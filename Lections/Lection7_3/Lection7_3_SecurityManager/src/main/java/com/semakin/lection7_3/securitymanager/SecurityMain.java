400d13308b1bc844b6db4aac5357a3407d786a78
Chi <chipsetone@gmail.com>
2017-02-15 13:11:38 +0300

    Попытки заставить работать эту хрень на паре. А зачем оно нам?

--- Lections/Lection7_3/Lection7_3_SecurityManager/src/main/java/com/semakin/lection7_3/securitymanager/SecurityMain.java
+++ Lections/Lection7_3/Lection7_3_SecurityManager/src/main/java/com/semakin/lection7_3/securitymanager/SecurityMain.java
@@ -10,13 +10,18 @@ public class SecurityMain {
     public static void main(String[] args) throws IOException {
         String fileName = "myFile.txt";
         MyWriter writer = new MyWriter(fileName);
-
+        System.out.println("started");
         writer.writeMy(10);
-
+        System.out.println("writed!");
         MyReader reader = new MyReader(fileName);
 
         int readedInt = reader.readMy();
 
-        System.out.println(readedInt);
+        System.out.println("readed: " + readedInt);
+
+        // TODO разобраться с read и write
     }
 }
+/*
+-Djava.security.manager -Djava.security.policy="D:\\tempGit\\git\\Innopolis\\Lections\\Lection7_3\\Lection7_3_SecurityManager\\java.policy"
+ */
