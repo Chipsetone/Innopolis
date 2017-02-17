9edf1b2f0b0e26fe5ec18523a50e6ab62e02e987
Chi <chipsetone@gmail.com>
2017-02-18 01:23:22 +0300

    Lection7_3_ClassLoader - создание инстанса в main

--- Lections/Lection7_3/Lection7_3_ClassLoader/src/main/java/Main.java
+++ Lections/Lection7_3/Lection7_3_ClassLoader/src/main/java/Main.java
@@ -1,16 +1,33 @@
+import com.semakin.classloader.UrlJarClassLoader;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+
 /**
  * @author Семакин Виктор
  */
 public class Main {
 
-    public static void main(String[] args) {
-        Animal animal = new Animal("beaver");
+    public static void main(String[] args) throws IllegalAccessException, InstantiationException, NoSuchMethodException, InvocationTargetException {
+        UrlJarClassLoader loader = new UrlJarClassLoader();
+        Class animalClass = loader.findClass("Animal");
+        Constructor constructor = animalClass.getConstructor(String.class);
+
+        Object objBeaver = constructor.newInstance("beaver");
+        Animal beaver = (Animal)objBeaver;
+       // ReflectionSerializer
+        System.out.println(animalClass);
+        System.out.println(beaver);
+
+        animalInAction(beaver);
+    }
+
+    private static void animalInAction(Animal animal){
 
         System.out.println("do something in main");
 
         animal.eat();
         animal.doCrap();
         animal.sleep();
-
     }
 }
