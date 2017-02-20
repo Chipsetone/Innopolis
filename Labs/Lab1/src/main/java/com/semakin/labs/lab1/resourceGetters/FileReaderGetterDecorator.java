0ae6a123ab26d775a6b5758231a3a59b64234582
Chi <chipsetone@gmail.com>
2017-02-20 04:49:42 +0300

    Lab1 - заготовка для использования библиотеки nio2

--- Labs/Lab1/src/main/java/com/semakin/labs/lab1/resourceGetters/FileReaderGetterDecorator.java
+++ Labs/Lab1/src/main/java/com/semakin/labs/lab1/resourceGetters/FileReaderGetterDecorator.java
@@ -2,8 +2,13 @@ package com.semakin.labs.lab1.resourceGetters;
 
 import com.semakin.labs.lab1.exceptions.InnerResourceException;
 
+import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.Reader;
+import java.nio.Buffer;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 /**
  * Получатель доступа к ресурсу файловой системы
@@ -19,13 +24,20 @@ public class FileReaderGetterDecorator extends ReaderGetterDecorator {
         super(readerGetter);
     }
 
+    @Override
+    public BufferedReader getBufferedReader(String resourceAddress) throws InnerResourceException {
+        return (BufferedReader)getResourceReader(resourceAddress);
+    }
+
     /**
      * {@inheritDoc}
      */
     @Override
     public final Reader getResourceReader(String resourceAddress) throws InnerResourceException {
         try{
-            return new FileReader(resourceAddress);
+            //return new FileReader(resourceAddress);
+            Path filePath = Paths.get(resourceAddress);
+            return Files.newBufferedReader(filePath);
         }
         catch(Exception ex){
             throw new InnerResourceException("Ошибка при работе с ресурсным файлом", ex);
